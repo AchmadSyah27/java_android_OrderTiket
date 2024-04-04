f69b9a5d115f915df6ee2ec9035a952f780ef53a
Achmad <achmadsyahhidayat@gmail.com>
2024-04-04 21:52:18 +0700

    commit pertama tiketSayaApp

---
+++ app/src/main/java/com/example/tiketsayaapp/SplashAct.java
@@ -0,0 +1,33 @@
+package com.example.tiketsayaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        //setting timer untuk 1 detik
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //merubah activity ke activity lain
+                Intent gogetstarted = new Intent(SplashAct.this,GetStartedAct.class);
+                startActivity(gogetstarted);
+                finish();
+            }
+        }, 1000); //1000ms = 1 detik
+    }
+}
