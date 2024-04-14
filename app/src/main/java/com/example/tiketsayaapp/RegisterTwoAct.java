d1f3722c3b6f535d1861bf0e40adde62d969f861
Achmad <achmadsyahhidayat@gmail.com>
2024-04-15 01:10:45 +0700

    add register two

---
+++ app/src/main/java/com/example/tiketsayaapp/RegisterTwoAct.java
@@ -0,0 +1,31 @@
+package com.example.tiketsayaapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegisterTwoAct extends AppCompatActivity {
+
+    LinearLayout btn_back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register_two);
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backtoprev = new Intent(RegisterTwoAct.this, RegisterOneAct.class);
+                startActivity(backtoprev);
+            }
+        });
+    }
+}
